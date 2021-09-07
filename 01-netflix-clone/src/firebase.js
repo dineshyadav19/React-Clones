75dda3c11edaeb605f20e5b2803a1d20ebdbc0ce
dineshyadav19 <dinesh.2108.yadav@gmail.com>
2021-09-07 14:22:46 +0530

    Sign up screen done

--- 01-netflix-clone/src/firebase.js
+++ 01-netflix-clone/src/firebase.js
@@ -1,3 +1,7 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore/lite';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
 const firebaseConfig = {
   apiKey: "AIzaSyD7Ic_4e1cc3AxnOQWoNm618sOeW0u1qEM",
   authDomain: "netflix-clone-39110.firebaseapp.com",
@@ -7,3 +11,10 @@ const firebaseConfig = {
   appId: "1:309113457858:web:9bd4b467d9ca6718523f5b",
   measurementId: "G-VBVXB0QPH7"
 };
+
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+const auth = getAuth();
+
+export { auth, createUserWithEmailAndPassword }
+export default db;
